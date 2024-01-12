174b5fa44da83fe1c0e7bbb38db4c1763801af00
AnthonyGardelle <a.gardelle18@gmail.com>
2024-01-12 09:25:06 +0100

    maj

--- festiuto/views.py
+++ festiuto/views.py
@@ -1,6 +1,31 @@
-from .app import app
+"""Module pour les vues de l'application."""
 from flask import render_template
+from .app import app
+from .form import SearchForm
 
 @app.route('/')
 def home() :
+    """Fonction de la vue de la page d'accueil.
+
+    Returns:
+        flask.reponse: Réponse de la page d'accueil.
+    """
     return render_template('home.html')
+
+@app.route("/search", methods =("POST",))
+def search() :
+    """Fonction de la vue de la page de recherche.
+
+    Returns:
+        flask.reponse: Réponse de la page de recherche.
+    """
+    form = SearchForm()
+    content_searched = form.searched.data
+    if content_searched == "":
+        return home()
+    return render_template (
+        "search.html",
+        form=form,
+        searched = content_searched,
+        title = "Search Page",
+    )
